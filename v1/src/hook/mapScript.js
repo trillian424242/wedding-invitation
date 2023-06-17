8ae8b01d31acdcb71eccc7ec322573095951352f
woncho <woncho@gmarket.com>
2023-06-17 23:02:10 +0900

    map level modified

--- v1/src/hook/mapScript.js
+++ v1/src/hook/mapScript.js
@@ -7,7 +7,7 @@ function renderMap() {
   var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
       mapOption = { 
           center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
-          level: 3 // 지도의 확대 레벨
+          level: 4 // 지도의 확대 레벨
       };
 
   var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
