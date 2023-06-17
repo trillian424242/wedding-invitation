2412dfa5b480491a686192aa42d1c1fa315b0d6a
woncho <woncho@gmarket.com>
2023-06-17 22:16:55 +0900

    API_KEY

--- v1/src/Config.ts
+++ v1/src/Config.ts
@@ -1,4 +1,4 @@
-export const WEDDING_INVITATION_URL = 'http://localhost:8080';
+export const WEDDING_INVITATION_URL = 'https://wedding-invitation-woad-tau.vercel.app';
 export const KAKAOTALK_API_TOKEN = '카카오톡 API 토큰을 입력해주세요!';
 export const KAKAOTALK_SHARE_IMAGE = '카카오톡으로 공유할 때 사용되는 대표이미지 URL을 입력해주세요!';
 
