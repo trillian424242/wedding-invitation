58bbba5f6e91d245b9e7f99e17c4409503cf5ea1
woncho <woncho@gmarket.com>
2023-06-18 16:15:39 +0900

    pictures

--- v1/src/Components/Gallery.tsx
+++ v1/src/Components/Gallery.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import { Divider } from 'antd';
 import styled from 'styled-components';
-import GalleryPhoto1 from '../Assets/photo1.png';
-import GalleryPhoto2 from '../Assets/photo2.png';
-import GalleryPhoto3 from '../Assets/photo3.png';
-import GalleryPhoto4 from '../Assets/Gallery_Photo_4.png';
-import GalleryPhoto5 from '../Assets/Gallery_Photo_5.png';
-import GalleryPhoto6 from '../Assets/Gallery_Photo_6.png';
+import GalleryPhoto1 from '../Assets/farm_1.jpg';
+import GalleryPhoto2 from '../Assets/farm_2.jpg';
+import GalleryPhoto3 from '../Assets/farm_3.jpg';
+import GalleryPhoto4 from '../Assets/farm_4.jpg';
+import GalleryPhoto5 from '../Assets/farm_5.jpg';
+import GalleryPhoto6 from '../Assets/farm_6.jpg';
+import GalleryPhoto7 from '../Assets/farm_7.jpg';
+import GalleryPhoto8 from '../Assets/sea_black and white.jpg';
+import GalleryPhoto9 from '../Assets/sea_black and white_2.jpg';
+import GalleryPhoto10 from '../Assets/sea_black and white_4.jpg';
+import GalleryPhoto11 from '../Assets/sea_black and white_5.jpg';
+import GalleryPhoto12 from '../Assets/(vertical)farm_.jpg';
+import GalleryPhoto13 from '../Assets/(vertical)farm_8.jpg';
+import GalleryPhoto14 from '../Assets/(vertical)farm_9.jpg';
+import GalleryPhoto15 from '../Assets/(vertical)sea_black and white_3.jpg';
 import GroovePaper from '../Assets/GroovePaper.png';
 
 const Wrapper = styled.div`
@@ -26,29 +35,50 @@ const Title = styled.p`
 
 const images = [
   {
-    original: GalleryPhoto1,
-    thumbnail: GalleryPhoto1,
+    original: GalleryPhoto1, thumbnail: GalleryPhoto1,
   },
   {
-    original: GalleryPhoto2,
-    thumbnail: GalleryPhoto2,
+    original: GalleryPhoto2, thumbnail: GalleryPhoto2,
   },
   {
-    original: GalleryPhoto3,
-    thumbnail: GalleryPhoto3,
+    original: GalleryPhoto3, thumbnail: GalleryPhoto3,
   },
   {
-    original: GalleryPhoto4,
-    thumbnail: GalleryPhoto4,
+    original: GalleryPhoto4, thumbnail: GalleryPhoto4,
   },
   {
-    original: GalleryPhoto5,
-    thumbnail: GalleryPhoto5,
+    original: GalleryPhoto5, thumbnail: GalleryPhoto5,
   },
   {
-    original: GalleryPhoto6,
-    thumbnail: GalleryPhoto6,
+    original: GalleryPhoto6, thumbnail: GalleryPhoto6,
   },
+  {
+    original: GalleryPhoto7, thumbnail: GalleryPhoto7,
+  },
+  {
+    original: GalleryPhoto8, thumbnail: GalleryPhoto8,
+  },
+  {
+    original: GalleryPhoto9, thumbnail: GalleryPhoto9,
+  },
+  {
+    original: GalleryPhoto10, thumbnail: GalleryPhoto10,
+  },
+  {
+    original: GalleryPhoto11, thumbnail: GalleryPhoto11,
+  },
+  {
+    original: GalleryPhoto12, thumbnail: GalleryPhoto12,
+  },
+  {
+    original: GalleryPhoto13, thumbnail: GalleryPhoto13,
+  },
+  {
+    original: GalleryPhoto14, thumbnail: GalleryPhoto14,
+  },
+  {
+    original: GalleryPhoto15, thumbnail: GalleryPhoto15,
+  }
 ];
 
 const Gallery = () => {
