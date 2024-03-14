ddf6523b28126eb5d88290bad981b0841c649a59
woncho <woncho@gmarket.com>
2024-03-14 21:31:13 +0900

    masking accounts

--- v1/src/Config.ts
+++ v1/src/Config.ts
@@ -5,15 +5,15 @@ export const WEDDING_DATE = '2023년 9월 23일, 토요일 오전 11시';
 export const WEDDING_LOCATION = '더 컨벤션 잠실점 비스타홀';
 
 export const GROOM_NAME = '조원일';
-export const GROOM_ACCOUNT_NUMBER = '신한 110-229-310814';
+export const GROOM_ACCOUNT_NUMBER = '신한 ***-***-******';
 export const GROOM_FATHER_NAME = '조유철';
-export const GROOM_FATHER_ACCOUNT_NUMBER = '우리 1002-442-377635';
+export const GROOM_FATHER_ACCOUNT_NUMBER = '우리 ****-***-******';
 export const GROOM_MOTHER_NAME = '박순석';
-export const GROOM_MOTHER_ACCOUNT_NUMBER = 'NH농협 122-12-053813';
+export const GROOM_MOTHER_ACCOUNT_NUMBER = 'NH농협 ***-**-******';
 
 export const BRIDE_NAME = '조윤진';
-export const BRIDE_ACCOUNT_NUMBER = '신한 110-554-268497';
+export const BRIDE_ACCOUNT_NUMBER = '신한 ***-***-******';
 export const BRIDE_FATHER_NAME = '조호진';
-export const BRIDE_FATHER_ACCOUNT_NUMBER = '신한 959-06-146829';
+export const BRIDE_FATHER_ACCOUNT_NUMBER = '신한 ***-**-******';
 export const BRIDE_MOTHER_NAME = '김정화';
 export const BRIDE_MOTHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
